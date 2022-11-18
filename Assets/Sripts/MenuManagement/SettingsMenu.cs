0a9ea4700d8e79d7c8d87167e9c8ca927027f0f5
Miguel Rodriguez <maiguelonflis456@gmail.com>
2022-11-18 21:10:06 +0100

    Tutorial adapted for different devices
    Los menus de controles y gameplay del tutorial llevan a diferentes pantallas dependiendo del dispositivo

--- Assets/Sripts/MenuManagement/SettingsMenu.cs
+++ Assets/Sripts/MenuManagement/SettingsMenu.cs
@@ -5,8 +5,29 @@ using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+    private bool desktop = true;
 
     public AudioMixer audioMixer;
+
+    [SerializeField] private GameObject PcControls;
+    [SerializeField] private GameObject MobileControls;
+
+    [SerializeField] private GameObject PcGameplay;
+    [SerializeField] private GameObject MobileGameplay;
+
+    private void Awake()
+    {
+        if (Application.isMobilePlatform)
+        {
+            desktop = false;
+        }
+        else if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            desktop = true;
+        }
+
+        //desktop = false;
+    }
 
     public void SetVolume(float volume) {
         audioMixer.SetFloat("volume", volume);
@@ -14,5 +35,53 @@ public class SettingsMenu : MonoBehaviour
 
     public void SetQuality(int qualityIndex) {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void ActivateControlsScreen()
+    {
+        if (desktop)
+        {
+            PcControls.SetActive(true);
+        }
+        else
+        {
+            MobileControls.SetActive(true);
+        }
+    }
+
+    public void DeactivateControlsScreen()
+    {
+        if (desktop)
+        {
+            PcControls.SetActive(false);
+        }
+        else
+        {
+            MobileControls.SetActive(false);
+        }
+    }
+
+    public void ActivateGameplayScreen()
+    {
+        if (desktop)
+        {
+            PcGameplay.SetActive(true);
+        }
+        else
+        {
+            MobileGameplay.SetActive(true);
+        }
+    }
+
+    public void DeactivateGameplayScreen()
+    {
+        if (desktop)
+        {
+            PcGameplay.SetActive(false);
+        }
+        else
+        {
+            MobileGameplay.SetActive(false);
+        }
     }
 }
