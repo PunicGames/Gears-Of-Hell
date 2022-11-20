1959c37c49270e0d85fd0327f27ab7ef7dc68117
Javisda <javiserrano97@hotmail.es>
2022-11-20 18:27:35 +0100

    Mejoras implementadas
    Se ponen los efectos de post procesado activados por defecto y se arreglan algunos errores.

--- Assets/Sripts/PostPorcessing/PostProcessingControler.cs
+++ Assets/Sripts/PostPorcessing/PostProcessingControler.cs
@@ -16,6 +16,22 @@ public class PostProcessingControler : MonoBehaviour
         volume.profile.TryGetSettings(out colorGrading);
     }
 
+    private void Start()
+    {
+        // Bloom
+        if (PlayerPrefs.GetInt("bloomEffect") == 0)
+            bloomEffect.active = false;
+        else
+            bloomEffect.active = true;
+
+        // Color Grading
+        if (PlayerPrefs.GetInt("colorGrading") == 0)
+            colorGrading.active = false;
+        else
+            colorGrading.active = true;
+
+    }
+
     public void UpdateBloom(bool opt) {
         bloomEffect.active = opt;
     }
