89626f601a0c09505da93c5ae3a6a87acffe33d8
Javisda <javiserrano97@hotmail.es>
2022-12-16 14:51:43 +0100

    WorkerFire (CP)
    Se implementa una escena y un comportamiento nuevo en el que un worker de tier 1 es capaz de apagar los fuegos originados por la araña pequeña. Creado para la asignatura de comportamiento de personajes.

--- Assets/Prefabs/Enemies/SpiderJavi/FireBehavior.cs
+++ Assets/Prefabs/Enemies/SpiderJavi/FireBehavior.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class FireBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform playerPos;
+    [SerializeField] private GameObject rangeToEnemy;
+    [SerializeField] private LayerMask m_LayerMask;
+    private WorkerBehavior m_Behavior;
+
+    private void Start()
     {
-        
+        playerPos = GameObject.FindGameObjectWithTag(nameof(Player)).GetComponent<Transform>();
+        InvokeRepeating("SendWorkerStopFire", 0.5f, 1f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SendWorkerStopFire() {
+        Collider[] hitColliders = Physics.OverlapSphere(rangeToEnemy.transform.position, rangeToEnemy.GetComponent<SphereCollider>().radius, m_LayerMask, QueryTriggerInteraction.Ignore);
+        foreach (Collider eC in hitColliders)
+        {
+            EnemyHealth eH = eC.GetComponent<EnemyHealth>();
+            if (eH.enemyType == EnemyHealth.EnemyType.WORKER && eC.GetComponent<FireWorker>() != null)
+            {
+                if (Vector3.Distance(eC.transform.position, playerPos.position) >= Vector3.Distance(eC.transform.position, this.transform.position))
+                {
+                    eC.GetComponent<FireWorker>().goFire = true;
+                    eC.GetComponent<FireWorker>().chasingTarget = transform.position;
+                    eC.GetComponent<FireWorker>().fireBehavior = this;
+                    break;
+                }
+                else { 
+                    eC.GetComponent<FireWorker>().goFire = false;
+                }
+            }
+        }
     }
+
+    public void Die() {
+        Destroy(gameObject);
+    }
+
 }
