bf9b534771d8d09e2260468f6cdd226a3de676bf
Javier Raja <iivaj@icloud.com>
2022-12-19 18:30:34 +0100

    BossFight PauseMenu

---
+++ Assets/Scenes/CP_Scenes/BossFight/BossFightManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossFightManager : MonoBehaviour
+{
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene("BossFight");
+    }
+}
